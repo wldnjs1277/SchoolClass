05cbd27870f70e83fb5fa5a12497a597ca98424a
wldnj <wldnjs1277@naver.com>
2022-08-08 11:10:01 +0900

    -view binding 알아보기

--- app/src/main/java/com/example/schoolclass/MainActivity.kt
+++ app/src/main/java/com/example/schoolclass/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.schoolclass
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import com.example.schoolclass.databinding.ActivityMainBinding
+
 
 private const val TAG = "MainActivity"
+
+private lateinit var binding:ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
     //액티비티가 생성되었을때
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
         //레이아웃을 설정
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         Log.d(TAG, "onCreate: ")
     }
 
@@ -27,6 +35,8 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         Log.d(TAG, "onPause: ")
+        binding.textView.visibility =View.VISIBLE
+        binding.textView.text="ON PAUSE()"
     }
 
     override fun onStop() {
