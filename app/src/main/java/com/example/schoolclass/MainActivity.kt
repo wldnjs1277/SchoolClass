395298473c7400c7b67cb75589862ae3a9f81372
wldnj <wldnjs1277@naver.com>
2022-09-26 15:57:30 +0900

    -

--- app/src/main/java/com/example/schoolclass/MainActivity.kt
+++ app/src/main/java/com/example/schoolclass/MainActivity.kt
@@ -36,7 +36,6 @@ class MainActivity : AppCompatActivity() {
         super.onPause()
         Log.d(TAG, "onPause: ")
         binding.textView.visibility =View.VISIBLE
-        binding.textView.text="ON PAUSE()"
     }
 
     override fun onStop() {
