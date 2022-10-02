91aede914b894d74c9fe8b84f251dfb4038d5950
wldnj <wldnjs1277@naver.com>
2022-10-02 16:21:54 +0900

    -

--- app/src/main/java/com/example/schoolclass/MainActivity.kt
+++ app/src/main/java/com/example/schoolclass/MainActivity.kt
@@ -7,7 +7,6 @@ import android.util.Log
 import android.view.View
 import com.example.schoolclass.databinding.ActivityMainBinding
 
-
 private const val TAG = "MainActivity"
 private lateinit var binding:ActivityMainBinding
 class MainActivity : AppCompatActivity() {
