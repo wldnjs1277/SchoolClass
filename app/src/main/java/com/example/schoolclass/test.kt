bb82e70240a8d781336bb654bec89432eb258dab
wldnj <wldnjs1277@naver.com>
2022-10-19 13:09:42 +0900

    -문제풀기

--- app/src/main/java/com/example/schoolclass/test.kt
+++ app/src/main/java/com/example/schoolclass/test.kt
@@ -1,17 +1,19 @@
 package com.example.schoolclass
 
 fun main(){
-println(solution(3))
+println(solution(12))
 }
-    fun solution(num: Int): String {
-        var answer = ""
-        if(num%2==0){
-            answer="Even"
-        }else{
-            answer="Odd"
+    fun solution(n: Int): Int {
+        var answer: Int = 0
+        for(x in 1..n) {
+            if (n.mod(x) == 1) {
+                answer = x
+                break
+            }
         }
         return answer
+    }
 
-}
+
 
 
